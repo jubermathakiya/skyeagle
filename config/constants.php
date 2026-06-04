b1528507962ae67934d74f58724e73b07a13bd60
juber <jgmathakiya21@gmail.com>
2026-06-04 18:34:16 +0530

    app name changes

--- config/constants.php
+++ config/constants.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'app_name' => env('APP_NAME', 'Skyeagle Trip'),
+
     'currency_symbol' => '₹',
     'backend_url' => env('BACKEND_URL'),
 ];
--- resources/views/layout/mainlayout.blade.php
+++ resources/views/layout/mainlayout.blade.php
@@ -9,7 +9,7 @@
         @hasSection('title')
             @yield('title') - 
         @endif
-        {{ env('APP_NAME') }}
+        {{ config('constants.app_name') }}
     </title>
     <meta name="description" content="DreamsTour - A premium Bootstrap 5 template crafted for travel and tour booking. Tailored for travel agencies and booking platforms, it features flight, hotel, and tour reservations, and holiday packages.">
     <meta name="keywords" content="travel booking template, tour booking, Bootstrap 5 travel template, DreamsTour, hotel booking, flights booking, holiday packages, tour agency website, travel agency template, travel HTML template, booking system, responsive travel template, Bootstrap travel website">
